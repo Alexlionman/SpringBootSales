6ef4d7d4dc881e1b5c5d94b92d4711e2e8de4114
467Ale467 <pereirasander33@gmail.com>
2022-06-20 16:22:58 -0300

    Initial Commit

---
+++ src/main/java/com/springbootsales/VendasApplication.java
@@ -0,0 +1,21 @@
+package com.springbootsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Online...";
+    }
+}
